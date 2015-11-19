1bb0378227d36d92399866d01795c024c69b071c
Klayton Faria <klayton.souza@gmail.com>
2015-11-19 17:40:05 -0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  entry: {
+    app: ['./app/main']
+  },
+  output: {
+    path: "./app",
+    filename: "app.js"
+  },
+  resolve: {
+    extensions: [
+      "",
+      ".js",
+      ".jsx",
+      ".scss"
+    ]
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css?sourceMap", "sass?sourceMap"]
+      }
+    ]
+  }
+};
