7ee02c3b5e2e6b9a0af51de716746a89a5712e13
Klayton Faria <klayton.souza@gmail.com>
2015-12-02 20:14:04 -0200

    developing…

---
+++ app/components/ui-Sidebar/sidebar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import css from './styles/_sidebar';
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.children = React.PropTypes.element;
+    this.state = {
+      collapsed: false
+    };
+  }
+  render() {
+    let isCollapsed = this.state.collapsed ? "collapsed" : "";
+
+    return (
+      <div id={this.props.id} className={"sidebar " + this.props.className + " " + isCollapsed}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
