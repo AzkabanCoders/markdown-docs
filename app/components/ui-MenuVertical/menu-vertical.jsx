53ff198734c68519c3e6205759fb62b9578c2bb4
Klayton Faria <klayton.souza@gmail.com>
2015-12-04 19:22:54 -0200

    Add menu flux structure

--- app/components/ui-MenuVertical/menu-vertical.jsx
+++ app/components/ui-MenuVertical/menu-vertical.jsx
@@ -1,41 +1,53 @@
 "use strict";
 
 import React, { Component } from 'react';
+import Store from "../../stores/menu-store";
 import css from './styles/_menu-vertical';
+
+let MenuStore = new Store();
+
+function getStateFromStore() {
+  return {
+    menu: MenuStore.getAll()
+  }
+}
 
 class MenuVertical extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      collapsed: false
+    };
+    this.state = getStateFromStore();
   }
+  onChange() {
+    this.setState(getStateFromStore());
+  }
+  // getInitialState() {
+  //   return getStateFromStore();
+  // }
+  componentDidMount() {
+    MenuStore.addChangeListener(this.onChange);
+  }
+  componentWillUnmount() {
+    MenuStore.removeChangeListener(this.onChange);
+  }
+
   render() {
+    var menuItems = this.state.menu.map(function(menu, index) {
+      return (
+        <li className='menu-item'  key={'menu-item-' + index}>
+          <a href="#">{menu}</a>
+        </li>
+      )
+    });
+
     return (
       <ul id={this.props.id} className={"menu-list " + this.props.className}>
         <li>
           <h3>Menu title</h3>
           <ul>
-            <li><a href="#">Item 1</a></li>
-            <li><a href="#">Item 2</a></li>
-            <li><a href="#">Item 3</a></li>
-          </ul>
-        </li>
-        <li>
-          <h3>Menu title 2</h3>
-          <ul>
-            <li>Item 1</li>
-          </ul>
-        </li>
-        <li>
-          <h3>Menu title</h3>
-          <ul>
-            <li><a href="#">Item 1</a></li>
-            <li><a href="#">Item 2</a></li>
-            <li><a href="#">Item 3</a></li>
-          </ul>
-        </li>
-        <li>
-          <h3>Menu title 2</h3>
-          <ul>
-            <li>Item 1</li>
+            {menuItems}
           </ul>
         </li>
       </ul>
---
+++ app/dispatcher/app-dispatcher.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import Flux, {Dispatcher} from 'flux';
+let _dispatcher = new Dispatcher();
+export default _dispatcher;
