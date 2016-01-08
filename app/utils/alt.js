f68a700b6985c68d0b574921fd59a92b9c3a5656
Klayton Faria <klayton.souza@gmail.com>
2016-01-08 11:08:41 -0200

    Updating and fixing app

--- app/utils/alt.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Alt = require('alt');
-var alt = new Alt();
-// var chromeDebug = require('alt/utils/chromeDebug')
-
-// chromeDebug(alt);
-
-module.exports = alt;
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,40 @@
 "use strict";
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    webpack = require("webpack");
 
 module.exports = {
   entry: {
-    app: ["./app/index"]
+    app: ["./app/index"],
+    vendor: ["react"]
   },
   output: {
     path: "./app",
     filename: "app.js"
   },
   resolve: {
-    extensions: [
-      "",
-      ".js",
-      ".jsx",
-      ".scss"
-    ]
+    extensions: ["", ".js", ".jsx", ".scss"]
   },
   devtool: "source-map",
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel',
+        loader: "babel",
         exclude: /node_modules/,
         query: {
-          presets: [
-            'es2015',
-            'react'
-          ]
+          presets: ["es2015", "react"]
         }
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css!sass')
-      },
-      {
-        test: /\.svg$/,
-        loader: 'file-loader'
+        loader: ExtractTextPlugin.extract("css!sass")
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('app.css', {
+    new ExtractTextPlugin("app.css", {
       allChunks: true
-    })
+    }),
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
   ]
 };
