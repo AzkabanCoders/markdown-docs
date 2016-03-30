138b13c3ec3df2af96d679d5c4c8597bb708a195
Klayton Faria <klayton.souza@gmail.com>
2016-03-30 19:29:04 -0300

    Update api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// import restful from "restful.js";
-
-// var api = restful().fullUrl("/resources/data.json");
 import constants from "../constants/";
 import restful, { fetchBackend } from "restful.js";
 const api = restful(constants.API_HOST, fetchBackend(fetch)).custom("resources/data.json");
