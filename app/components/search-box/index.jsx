03fa4d36c22f6b5dbaad544c95ebda19a58cb843
Klayton Faria <klayton.souza@gmail.com>
2016-04-07 21:29:13 -0300

    Update search

--- app/components/search-box/index.jsx
+++ app/components/search-box/index.jsx
@@ -9,7 +9,10 @@ class SearchBox extends Component {
   render() {
     return (
       <div className="search-box">
-        <span className="icon"><SVGInline svg={Icon} fill="#FFF" cleanup={true}  with={28} height={28}/></span>
+        <div className="search-wrapper">
+          <SVGInline svg={Icon} className="icon-search" fill="#FFF" cleanup={true} height="22px"/>
+          <input type="search" placeholder="Search..." />
+        </div>
       </div>
     );
   }
