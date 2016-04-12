64a0fb4e42eb8da38c9e170fef3b8e51d92af8b4
Klayton Faria <klayton.souza@gmail.com>
2016-04-12 15:31:20 -0300

    Update searchbox

--- app/components/header/index.jsx
+++ app/components/header/index.jsx
@@ -6,13 +6,28 @@ import ImageLoader from "../utils/image-loader";
 import SearchBox from "../search-box";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.data !== this.props.data) {
+      this.setState({
+        data: this.props.data
+      });
+    }
+  }
+
   render() {
     return (
       <header id={this.props.id} className='noselect'>
         <h1 className="logo">
           <ImageLoader src={this.props.logo} href="/" height="40" />
         </h1>
-        <SearchBox autocomplete={true} />
+        <SearchBox autocomplete={true} data={this.state.data} />
       </header>
     );
   }
