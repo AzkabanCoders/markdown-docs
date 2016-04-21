9b375c4eda0e1e71354f2a1cb5f52d6ca3568676
Klayton Faria <klayton.souza@gmail.com>
2016-04-20 21:24:41 -0300

    Put content on iframe

--- app/components/component-page.jsx
+++ app/components/component-page.jsx
@@ -1,6 +1,7 @@
 "use strict";
 
 import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
 import Utils from "../utils";
 import Styles from "./utils/styles-loader";
 import Scripts from "./utils/scripts-loader";
@@ -8,30 +9,50 @@ import Highlight from "react-highlight";
 
 // Components
 import Main from '../main';
-
+var teste;
 class ComponentPageContent extends Component {
   constructor(props) {
     super(props);
   }
 
   componentDidUpdate() {
-    // $(document).trigger("applyHighlight");
+
+    console.log("init", "componentDidUpdate");
+    var iframe = document.getElementById("content-frame"),
+        iDocument = iframe.contentWindow.document;
+
+
+        iDocument.open();
+        iDocument.write(teste.props.children);
+        iDocument.close();
+        console.log("end","componentDidUpdate");
+
+        // var el = document.createElement("div");
+        // ReactDOM.render(iframeContent, el);
+
+
+      console.log("el", teste.props.children);
+
+
   }
 
   render() {
     // Data props comes from Main context
     let data = Utils.content.get(this.props.data, this.props.contentId),
         styles = data.styles || [],
-        scripts = data.scripts || [];
-
+        scripts = data.scripts || [],
+        htmlContent = <Highlight innerHTML={true}>{data.contents}</Highlight>;
+          teste = htmlContent;
+        var frame = <iframe id="content-frame" width="100%" frameBorder="0"></iframe>;
     return (
       <div>
         <Styles data={styles} />
         <h2 className="component-name-title">{data.title}</h2>
         {/*<div className="content" dangerouslySetInnerHTML={{__html: data.contents}}></div>*/}
-        <Highlight innerHTML={true}>
-          {data.contents}
-        </Highlight>
+        <Styles data={styles} />
+        {/*<div id="contents" data-content={htmlContent.props.children}></div>*/}
+        {/*{frame}*/}
+        {htmlContent}
         <Scripts data={scripts} />
       </div>
     );
