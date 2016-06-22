d34cb479ccbff44af5b4ccbd3081988fe88aa0bf
Klayton Faria <klayton.souza@gmail.com>
2016-06-22 10:05:17 -0300

    Update route file location

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import {Router, Route} from 'react-router/umd/ReactRouter';
-import {createHashHistory} from 'history'
+import {createHashHistory} from 'history';
 
 // Components
-import ComponentPage from '../components/component-page';
-import IndexPage from '../components/index-page';
-import ErrorPage from '../components/error-page';
+import ComponentPage from './components/component-page';
+import IndexPage from './components/index-page';
+import ErrorPage from './components/error-page';
 
 var history = createHashHistory({
   queryKey: false
@@ -14,6 +14,8 @@ var history = createHashHistory({
 export default (
   <Router history={history}>
       <Route path='/' component={ComponentPage}>
+        <Route path=':componentId' component={ComponentPage}/>
+        <Route path='docs/:componentId' component={ComponentPage}/>
         <Route path='docs/:section/:componentId' component={ComponentPage}/>
       </Route>
       <Route path='*' component={ErrorPage} />
