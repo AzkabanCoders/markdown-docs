80bdea658b8bd465e2911715bff36b2aeb15e6be
Klayton Faria <klayton.souza@gmail.com>
2016-06-22 10:05:17 -0300

    Update route file location

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,6 @@
 "use strict";
 
-import React, {Component} from "react";
+import routes from "./routes";
 import ReactDOM, {render} from "react-dom";
-import routes from "./routes/routes";
 
 render(routes, document.getElementById("app"));
