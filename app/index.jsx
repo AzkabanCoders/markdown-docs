1252ef8eaa51b37e54617de06e2173c5fce35d20
Klayton Faria <klayton.souza@gmail.com>
2016-07-04 15:19:12 -0300

    Add service worker (wip)

--- app/index.jsx
+++ app/index.jsx
@@ -4,3 +4,17 @@ import routes from "./routes";
 import ReactDOM, {render} from "react-dom";
 
 render(routes, document.getElementById("app"));
+
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: './../'
+  }).then(function(registration) {
+    if (typeof registration.update == 'function') {
+      registration.update();
+    }
+  }).catch(function(e) {
+    console.error('Error during service worker registration:', e);
+  });
+}
